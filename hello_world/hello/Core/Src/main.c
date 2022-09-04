691cd3eb6578e91b57888e89e6a53cc7d66a5efd
Kate <avsyanik.ekaterina@gmail.com>
2022-09-04 08:55:58 +0300

    change formatting

--- hello_world/hello/Core/Src/main.c
+++ hello_world/hello/Core/Src/main.c
@@ -56,9 +56,9 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	if (GPIO_Pin == GPIO_PIN_13) {
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	}
+  if (GPIO_Pin == GPIO_PIN_13) {
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+  }
 }
 /* USER CODE END 0 */
 
@@ -199,7 +199,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
